fa26870279504ad05361f6cd2236b03271be9f57
Eduardo P Montenegro <edupmon@outlook.com>
2024-07-03 16:36:20 -0300

    Creating a card view

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MeetingView.swift
 //  Scrumdinger
 //
 //  Created by Eduardo Montenegro on 03/07/24.
@@ -13,19 +13,22 @@ struct MeetingView: View {
             ProgressView(value: 5, total: 15)
             HStack {
                 VStack(alignment: .leading) {
-                    Text("Seconds Elapsed").font(.caption)
+                    Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.tophalf.fill")
                 }
                 Spacer()
                 VStack(alignment: .trailing) {
-                    Text("Seconds Remaining").font(.caption)
+                    Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
             .accessibilityElement(children: .ignore)
             .accessibilityLabel("Time remaining")
             .accessibilityValue("10 minutes")
-            Circle().strokeBorder(lineWidth: 24)
+            Circle()
+                .strokeBorder(lineWidth: 24)
             HStack {
                 Text("Speaker 1 of 3")
                 Spacer()
@@ -39,6 +42,8 @@ struct MeetingView: View {
     }
 }
 
-#Preview {
-    MeetingView()
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
 }
